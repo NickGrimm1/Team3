1d914226c67e3a0be39c0c6c07c3d943d8749740
sh3rifme <sh3rifme@gmail.com>
2015-02-04 14:27:53 +0000

    Cleaned up framework, files in relevant folders

--- Team3Project1/Renderer.h
+++ Team3Project1/Renderer.h
@@ -1,10 +1,18 @@
 #pragma once
-#include "Framework\OGLRenderer.h"
+#include "../Framework/OGLRenderer.h"
 
 class Renderer : public OGLRenderer
 {
 public:
 	Renderer(void);
 	~Renderer(void);
-};
 
+	void Render(float msec);
+private:
+	void RenderScene(float msec);
+	//void AddLight(Light arg);
+	//void RemoveLight(Light arg);
+	void ShadowPass();
+	void MotionBlurPass();
+	void BloomPass();
+};
