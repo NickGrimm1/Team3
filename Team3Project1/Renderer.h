fe851d26cf32789feeb92c250ddcf86f7c87d3a2
sh3rifme <sh3rifme@gmail.com>
2015-02-04 15:23:36 +0000

    Meat of the renderer declarations in header

--- Team3Project1/Renderer.h
+++ Team3Project1/Renderer.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "../Framework/Light.h"
+#include "../Framework/Texture.h"
 #include "../Framework/OGLRenderer.h"
 
 class Renderer : public OGLRenderer
@@ -7,12 +9,38 @@ public:
 	Renderer(void);
 	~Renderer(void);
 
-	void Render(float msec);
+	void		Render(float msec);
+
+	unsigned int AddLight(Light &arg);
+	Light&		GetLight(int arg);
+	void		RemoveLight(int arg);
+
+	unsigned int AddTexture(Texture &arg);
+	Texture&	GetTexture(int arg);
+	void		RemoveTexture(int arg);
+
+	unsigned int AddMesh(Mesh &arg);
+	Mesh&		GetMesh(int arg);
+	void		RemoveMesh(int arg);
+
 private:
-	void RenderScene(float msec);
-	//void AddLight(Light arg);
-	//void RemoveLight(Light arg);
-	void ShadowPass();
-	void MotionBlurPass();
-	void BloomPass();
+	//Start of render pipeline
+	void		RenderScene(float msec);
+	
+	//Rendering pipeline components.
+	void		ShadowPass();
+	void		DeferredLightPass();
+	void		MotionBlurPass();
+	void		BloomPass();
+
+	//Member variables.
+
+	Texture*	texArray[10];
+	//Light		lightArray[10];
+	Mesh*		meshArray[10];
+
+	Shader*		basicShader, shadowShader, bloomShader, deferredShader, blurShader, combineShader;
+
+
+
 };
