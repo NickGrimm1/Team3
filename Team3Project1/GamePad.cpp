9c767e99c0b949766416543fc0a7e54a85b6fc92
MatthewAJohnson <matthew.johnson1@ncl.ac.uk>
2015-02-05 17:16:27 +0000

    Outline for ps3 gamepad class

---
+++ Team3Project1/GamePad.cpp
@@ -0,0 +1,47 @@
+#include "GamePad.h"
+
+float GamePad::pointerPosX = 0.0f;
+float GamePad::pointerPosY = 0.0f;
+
+float GamePad::pointerRelativeX = 0.0f;
+float GamePad::pointerRelativeY = 0.0f;
+
+PadInput GamePad::padFunctions[PADBUTTONS_MAX];
+
+void GamePad::Initialise()
+{
+	/*
+	if(cellPadInit(1) != CELL_PAD_OK)
+	{
+		std::cout <<"cellPadInit failed!" << std::endl;
+	}
+
+	for(int i = 0; i < NUM_FUNCTIONS; ++i) //num functions may be supposed to be PADBUTTONS_MAX
+	{
+		padFunctions[i] = NULL;
+	}
+	*/
+}
+
+void GamePad::Destroy()
+{
+	//cellPadEnd();
+}
+
+void GamePad::GetPointerPosition(float &x, float &y)
+{
+	x = pointerPosX;
+	y = pointerPosY;
+}
+
+void GamePad::GetJoypadMovement(float &x, float &y)
+{
+	x = pointerRelativeX;
+	y = pointerRelativeY;
+}
+
+
+void GamePad::SetPadFunction(PadButtons button, PadInput function)
+{
+	padFunctions[button] = function;
+}
