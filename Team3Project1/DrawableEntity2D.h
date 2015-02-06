883bbd5603b5fc04c814329a91eb233cc8dc6a6c
NickGrimm1 <n.grimm@ncl.ac.uk>
2015-02-06 12:23:07 +0000

    Added drawable Text. Couple of header files moved to different filters.

--- Team3Project1/DrawableEntity2D.h
+++ Team3Project1/DrawableEntity2D.h
@@ -1,12 +1,28 @@
 /**
+Version History:
+0.0.5 05/02/2015
+0.0.6 06/02/2015
+	Added Type.
+*/
+
+/**
 <summary>Defines an object that can be drawn to the screen.
 
 Author: Nick Grimm
-Version: 0.0.5 05/02/2015.</summary>
+Version: 0.0.6 06/02/2015.</summary>
 */
 #pragma once
 #include "T3Rectangle.h"
 #include "../Framework/Vector4.h"
+
+namespace DrawableType
+{
+	enum Type
+	{
+		Texture,
+		Text,
+	};
+}
 
 class DrawableEntity2D : public T3Rectangle
 {
@@ -19,8 +35,13 @@ public:
 	<param name='height'>The height of the entity.</param>
 	<param name='blendColor'>The colour. Default is White (1,1,1,1).</param>
 	*/
-	DrawableEntity2D(float x, float y, float width, float height, Vector4 blendColor = Vector4(1.0f, 1.0f, 1.0f ,1.0f)) : T3Rectangle(x, y, width, height), blendColor(blendColor)
+	DrawableEntity2D(float x, float y, float width, float height, DrawableType::Type type, Vector4 blendColor = Vector4(1.0f, 1.0f, 1.0f ,1.0f)) : T3Rectangle(x, y, width, height), blendColor(blendColor), type(type)
 	{ }
+
+	/**
+	<summary>Gets the drawable type of this object.</summary>
+	*/
+	DrawableType::Type GetType() { return type; }
 	/**
 	<summary>Gets the colour to blend this drawable with.</summary>
 	*/
@@ -32,4 +53,5 @@ public:
 	void SetColor(const Vector4& value) { blendColor = value; }
 protected:
 	Vector4 blendColor;
+	DrawableType::Type type;
 };
