6859f682974a41c0f4e7105f7c979b6b2d4754b5
TerryXu01 <j.xu15@newcastle.ac.uk>
2015-03-03 16:36:48 +0000

    Set listener position in audio engine

--- Team3Project1/EntryPoint.h
+++ Team3Project1/EntryPoint.h
@@ -22,14 +22,14 @@ namespace EntryPoint
 	void StartGame()
 	{
 		// TODO: Get a new gameScreen up for splash/menu etc.
-		//LoadingScreen* loadingScreen = new LoadingScreen();
-		//GameStateManager::Instance()->AddGameScreen(loadingScreen);
+		LoadingScreen* loadingScreen = new LoadingScreen();
+		GameStateManager::Instance()->AddGameScreen(loadingScreen);
 
 		GraphicsTestScreen* game = new GraphicsTestScreen();
 		GameStateManager::Instance()->AddGameScreen(game);
 
-		//HudTestScreen* hud = new HudTestScreen();
-		//GameStateManager::Instance()->AddGameScreen(hud);
+		HudTestScreen* hud = new HudTestScreen();
+		GameStateManager::Instance()->AddGameScreen(hud);
 
 		DebugOverlay* debug = new DebugOverlay();
 		GameStateManager::Instance()->AddGameScreen(debug);
@@ -38,8 +38,8 @@ namespace EntryPoint
 		GameStateManager::Instance()->AddGameScreen(audio);
 
 
-		//RacerGame* game = new RacerGame();
-		//GameStateManager::Instance()->AddGameScreen(game);
+//		RacerGame* game = new RacerGame();
+//		GameStateManager::Instance()->AddGameScreen(game);
 		GameStateManager::Instance()->Start();
 	}
 }
