7ea47ef4547f199b659ee50338655631f9c899e8
JustTh1sGuy <dkelly3_16@hotmail.com>
2015-03-05 17:22:36 +0000

    Adding scoreboard class

---
+++ Team3Project1/Scoreboard.h
@@ -0,0 +1,42 @@
+#pragma once
+#ifdef WINDOWS_BUILD
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class BaseSocket;
+
+class Scoreboard
+{
+public:
+	Scoreboard(void);
+	~Scoreboard(void);
+
+	// Web requests
+	bool RetrieveScoreboard();
+	bool PostScore(string name, unsigned int score);
+
+	// Local requests
+	bool IsScoreboardLoaded() const;
+	unsigned int GetLowestScore() const;
+	string GetName(unsigned int position) const;
+	string GetScore(unsigned int position) const;
+	unsigned int GetSize() const;
+
+protected:
+	bool ConnectToScoreboard();
+
+
+	bool scoreboardConnected;
+	bool scoreboardRequestSent;
+	bool scoreboardRequestSucceeded;
+	bool scoreboardRetrieved;
+	vector<pair<string, string>> scoreboard; //<name, score>
+
+	string scoreboardHost;
+	BaseSocket* scoreboardDB;
+};
+
+#endif
